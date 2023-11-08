d2a6d9350350155e7650fe76cef81ffd527618d0
ngodingbentar <dewaqintoro@gmail.com>
2023-11-08 19:03:13 +0700

    Added: react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/home'
+import ErrorPage from './pages/404'
+import ProfilePage from './pages/profile'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/profile',
+    element: <ProfilePage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
